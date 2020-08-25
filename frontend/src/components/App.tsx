192c4f399eb1b03daf9c398d7ae74013cc8804dc
Logvinenko Dmitriy <dmitriy.logvinenko@ramax.ru>
2020-08-25 22:49:47 +0300

    Create Auth

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Auth from './auth/Auth';
 
 type TApp = unknown
 
 export const App: React.FC<TApp> = () => {
     return (
-        <div className="container">
-            <h1>Hello world</h1>
-        </div>
+        <Auth />
     )
 }
--- frontend/src/components/auth/Auth.tsx
+++ frontend/src/components/auth/Auth.tsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-export const Auth: React.FC = () => {
+export default function Auth(){
     return (
-        <div className="container">
-            <div>
-
+        <div className="auth__body">
+            <div className="auth__form">
+                <div className="auth__title">
+                    <div>
+                        <i className="fas fa-graduation-cap"></i>
+                    </div>
+                    <div>
+                        school crm
+                    </div>
+                </div>
+                <div className="auth__input">
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email или Логин"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                </div>
+                <div className="auth__input">
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="password"
+                        label="Пароль"
+                        name="password"
+                        autoComplete="password"
+                        autoFocus
+                    />
+                </div>
+                <div className="auth__input">
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                    >
+                        Войти
+                    </Button>
+                </div>
+                
             </div>
         </div>
-    )
+    );
 }
