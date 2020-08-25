6fceb1de3f3cadf5dbf84556e14d482cee1c588a
sood21-js <54079529+sood21-js@users.noreply.github.com>
2020-08-25 22:51:32 +0300

    Create Auth (#2)
    Co-authored-by: Logvinenko Dmitriy <dmitriy.logvinenko@ramax.ru>

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,5 @@
 import './styles/style.css';
 import './styles/style.scss';
-import './styles/style.less';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import 'materialize-css/dist/js/materialize.min';
